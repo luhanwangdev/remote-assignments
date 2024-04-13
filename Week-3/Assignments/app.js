93e10f14e0d8915ca933e1907cba0c008edbda10
Lu-Han Wang <r10521523@ntu.edu.tw>
2024-04-13 11:55:49 +0800

    feat: set up the data routes

--- Week-3/Assignments/app.js
+++ Week-3/Assignments/app.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const app = express();
 
+//setting router
 const mainRoutes = require("./routers");
+const dataRoutes = require("./routers/data");
 app.use(mainRoutes);
+app.use("/data", dataRoutes);
 
 app.listen(3000, () => {
   console.log("The server is on the port 3000");
