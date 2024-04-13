4555f351ce5ba5fc419e1fd637eda44a4cecfa28
Lu-Han Wang <r10521523@ntu.edu.tw>
2024-04-13 12:33:06 +0800

    feat: create /public folder

--- Week-3/Assignments/app.js
+++ Week-3/Assignments/app.js
@@ -7,6 +7,8 @@ const dataRoutes = require("./routers/data");
 app.use(mainRoutes);
 app.use("/data", dataRoutes);
 
+app.use(express.static("public"));
+
 app.listen(3000, () => {
   console.log("The server is on the port 3000");
 });
