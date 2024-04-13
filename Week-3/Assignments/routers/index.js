5137d8c8d5236b326d3d7dab4c07ca6264d22169
Lu-Han Wang <r10521523@ntu.edu.tw>
2024-04-13 14:22:39 +0800

    style: set 'Hello, my server' to h1 tag

--- Week-3/Assignments/routers/index.js
+++ Week-3/Assignments/routers/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/", (req, res) => {
-  res.send("Hello, My Server!");
+  res.send("<h1>Hello, My Server!</h1>");
 });
 
 module.exports = router;
