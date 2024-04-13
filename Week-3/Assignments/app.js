b3181ce9e6e4c457502d9d3108b96a464a866ecd
Lu-Han Wang <r10521523@ntu.edu.tw>
2024-04-13 14:54:36 +0800

    feat: update routers 'myName' and 'trackName'

--- Week-3/Assignments/app.js
+++ Week-3/Assignments/app.js
@@ -4,8 +4,13 @@ const app = express();
 //setting router
 const mainRoutes = require("./routers");
 const dataRoutes = require("./routers/data");
+const myNameRoutes = require("./routers/myName");
+const trackNameRoutes = require("./routers/trackName");
+
 app.use(mainRoutes);
 app.use("/data", dataRoutes);
+app.use("/myName", myNameRoutes);
+app.use("/trackName", trackNameRoutes);
 
 app.use(express.static("public"));
 
