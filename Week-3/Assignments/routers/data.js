70f71494659a29eb3d86c0e79c7f3730c26f9db4
Lu-Han Wang <r10521523@ntu.edu.tw>
2024-04-13 12:24:26 +0800

    feat: finish data routers with query string

--- Week-3/Assignments/routers/data.js
+++ Week-3/Assignments/routers/data.js
@@ -2,7 +2,25 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/", (req, res) => {
-  res.send("Here is stand for the data ");
+  const { number } = req.query;
+  if (!number) {
+    res.send("Lack of Parameter");
+  } else if (number % 1 !== 0 || number / 1 <= 0) {
+    //stand for non-integer and negative integer and 0
+    res.send("Wrong parameter");
+  } else {
+    //recursion
+    const sum = (n) => {
+      if (n === 0) {
+        return 0;
+      } else {
+        return n + sum(n - 1);
+      }
+    };
+    //change the number from string to integer
+    const int = number / 1;
+    res.send(`${sum(int)}`);
+  }
 });
 
 module.exports = router;
