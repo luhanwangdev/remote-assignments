90a2525b4469ae5425228fc2784f6e951734a1ec
Lu-Han Wang <r10521523@ntu.edu.tw>
2024-04-18 14:33:11 +0800

    feat: create home page

--- Week-4/Assignment-3/app.js
+++ Week-4/Assignment-3/app.js
@@ -1,5 +1,11 @@
 import express from "express";
-const app = express;
+const app = express();
+
+app.set("view engine", "pug");
+
+app.get("/", (req, res) => {
+  res.render("index");
+});
 
 app.listen(3000, () => {
   console.log("The server is now listening on port 3000");
