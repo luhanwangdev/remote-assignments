8beb5d82407ca1d555ad7c6e440bbdba09c34f17
Lu-Han Wang <r10521523@ntu.edu.tw>
2024-04-18 12:51:51 +0800

    feat: complete C.js

--- Week-4/Assignment-1/C.js
+++ Week-4/Assignment-1/C.js
@@ -1,5 +1,13 @@
 async function main() {
   // your code here, you should call delayedResultPromise here and get the result using async/await.
+  function delayedResultPromise(n1, n2, delayTime) {
+    return new Promise((resolve) => {
+      const result = n1 + n2;
+      setTimeout(resolve, delayTime, result);
+    });
+  }
+
+  const show = await delayedResultPromise(4, 5, 3000);
+  console.log(show);
 }
-main(); // result will be shown in the console after <delayTime>
-seconds;
+main(); // result will be shown in the console after <delayTime> seconds
