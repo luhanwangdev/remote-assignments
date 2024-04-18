9c40ff58caffe939b0f42656b80a9213cc06b31e
Lu-Han Wang <r10521523@ntu.edu.tw>
2024-04-18 12:37:38 +0800

    feat: complete B.js

--- Week-4/Assignment-1/B.js
+++ Week-4/Assignment-1/B.js
@@ -1,5 +1,9 @@
 function delayedResultPromise(n1, n2, delayTime) {
   // your code here
+  return new Promise((resolve, reject) => {
+    const result = n1 + n2;
+    setTimeout(resolve, delayTime, result);
+  });
 }
 delayedResultPromise(4, 5, 3000).then(console.log);
 // 9 (4+5) will be shown in the console after 3 seconds
