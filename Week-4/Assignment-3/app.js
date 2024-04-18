a487d62747325d302c6b1ab939b2ec2c9e882709
Lu-Han Wang <r10521523@ntu.edu.tw>
2024-04-18 16:59:12 +0800

    feat: complete sign-up form

--- Week-4/Assignment-3/app.js
+++ Week-4/Assignment-3/app.js
@@ -1,10 +1,38 @@
 import express from "express";
+import cookieParser from "cookie-parser";
+import bodyParser from "body-parser";
+import { getUsers, getUser, createUser } from "./database.js";
+
 const app = express();
 
 app.set("view engine", "pug");
+app.use(cookieParser());
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.get("/", (req, res) => {
   res.render("index");
+});
+
+app.post("/signUp", async (req, res) => {
+  const { email, password } = req.body;
+  const result = await createUser(email, password);
+  console.log(result);
+  res.redirect("member");
+});
+
+app.post("/signIn", async (req, res) => {
+  const { email, password } = req.body;
+
+  res.redirect("member", { email });
+});
+
+app.get("/member", (req, res) => {
+  res.render("member");
+});
+
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send("Something Broke!");
 });
 
 app.listen(3000, () => {
--- Week-4/Assignment-3/schema.sql
+++ Week-4/Assignment-3/schema.sql
@@ -3,7 +3,7 @@ USE assignment;
 
 CREATE TABLE `user`(
     id INTEGER PRIMARY KEY AUTO_INCREMENT,
-    email VARCHAR(255) NOT NULL,
+    email VARCHAR(255) UNIQUE NOT NULL,
     `password` VARCHAR(255) NOT NULL
 );
 
