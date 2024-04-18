fb32ee6440a8da6806efdbc4e916e7492aab6511
Lu-Han Wang <r10521523@ntu.edu.tw>
2024-04-18 14:06:13 +0800

    feat: create database schema

---
+++ Week-4/Assignment-3/schema.sql
@@ -0,0 +1,12 @@
+CREATE DATABASE assignment;
+USE assignment;
+
+CREATE TABLE `user`(
+    id INTEGER PRIMARY KEY AUTO_INCREMENT,
+    email VARCHAR(255) NOT NULL,
+    `password` VARCHAR(255) NOT NULL
+);
+
+INSERT INTO `user`(email, `password`)
+VALUES
+("123@gmail.com", "123");
